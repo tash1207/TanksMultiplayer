646611d53ac77691c9a655f5b4bb38e60639b251
Tasha Hankewych <thankewych@gmail.com>
2025-03-20 21:55:45 -0500

    Display the player's health above their tank

---
+++ Assets/Scripts/Core/Combat/HealthDisplay.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : NetworkBehaviour
+{
+    [Header("References")]
+    [SerializeField] Health health;
+    [SerializeField] Image healthBarImage;
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsClient) { return; }
+
+        health.CurrentHealth.OnValueChanged += HandleHealthChanged;
+        HandleHealthChanged(0, health.CurrentHealth.Value);
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (!IsClient) { return; }
+
+        health.CurrentHealth.OnValueChanged -= HandleHealthChanged;
+    }
+
+    private void HandleHealthChanged(int oldHealth, int newHealth)
+    {
+        health.CurrentHealth.Value = newHealth;
+        healthBarImage.fillAmount = (float) newHealth / health.MaxHealth;
+    }
+}
